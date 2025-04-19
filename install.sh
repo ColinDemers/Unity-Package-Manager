4a0422addaa88c87a1a6a8d07fe4f34e252200c7
ColinDemers <colin.demers739@gmail.com>
2025-04-19 15:08:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 curl -L -o main https://github.com/ColinDemers/Unity-Package-Manager/releases/latest/download/main
 chmod +x main
 sudo mv main /usr/local/bin/
+rn /usr/local/bin/main /usr/local/bin/upm
 
 echo "✅ Installed, run upm -h to begin."
