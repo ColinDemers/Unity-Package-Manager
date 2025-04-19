67984ae5f59c1b90b6b14fdc2f53bbdd6fd4db17
ColinDemers <colin.demers739@gmail.com>
2025-04-19 15:00:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o unipkg https://github.com/ColinDemers/Unity-Package-Manager/releases/latest/download/main
+curl -L -o main https://github.com/ColinDemers/Unity-Package-Manager/releases/latest/download/main
 chmod +x main
 sudo mv main /usr/local/bin/
 
